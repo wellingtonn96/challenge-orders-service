0eda557ebd21ea66bebc0f737f806fc828270018
wellington santos <weltossousa@gmail.com>
2026-09-16 13:58:09 -0300

    chore: add initial structure project

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { Order } from './order/entities/order.entity';
+import { SharedModule } from './shared/shared.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST ?? 'localhost',
+      port: Number(process.env.DATABASE_PORT ?? 5432),
+      username: process.env.DATABASE_USER ?? 'orders',
+      password: process.env.DATABASE_PASSWORD ?? 'orders',
+      database: process.env.DATABASE_NAME ?? 'orders',
+      entities: [Order],
+      synchronize: process.env.TYPEORM_SYNC !== 'false',
+      logging: process.env.TYPEORM_LOGGING === 'true',
+    }),
+    SharedModule,
+    OrderModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
